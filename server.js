94da6c4084b16021959b06a2e7559f4f3b675bbb
paulina koch <pjassakoch@gmail.com>
2025-05-09 12:34:18 -0400

    Connected mongoDb and installed mongoose

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 import router from './routes/whiteboardRoutes.js';
 
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
 
 const app = express();
 const PORT = process.env.PORT || 5050;
@@ -11,9 +16,9 @@ const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
+
+//Routes
 app.use("/api/whiteboard", router);
-
-
 
 
 app.listen(PORT, () => {
