89f5e559cc1c41e6b18fb36540c60b580684b0e6
paulina koch <pjassakoch@gmail.com>
2025-08-12 15:17:20 -0400

    Added a Login component

--- client/src/componenets/Login.js
+++ client/src/componenets/Login.js
@@ -4,5 +4,38 @@ export default function Login({ onLogin}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    
-}
+    const handleSubmit = async () => {
+        const res = await fetch('/api/users/login', {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+
+        });
+
+        if( res.ok) {
+            const { token } = await res.json();
+            onLogin(token);
+        } else {
+            alert ('Invalid credentials');
+
+        }
+        };
+
+        return (
+           <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <input 
+                placeholder="Email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                type="password"
+                placeholder='Password'
+                value={password}
+                onChange={ (e) => setPassword(e.target.value)}
+                 />
+                 <button type="submit" >Login</button>
+         </form>
+        )
+    }
