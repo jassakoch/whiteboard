ab0af39b395a8d2a9a5ba5898cd744438cfd2179
paulina koch <pjassakoch@gmail.com>
2025-08-12 07:34:50 -0400

    Working on Login componenet in fontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 
 import './App.css';
+import { useState } from 'react';
+import  Login from './components/Login'
 
 function App() {
+
+  const [ token, setToken ] = useState(null);
+
+  if (!token) {
+    return <Login onLogin={setToken} />;
+  }
   return (
     <div className="App">
 <h1>Scribble</h1>
