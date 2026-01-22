9d6986282e7c7317137a53af95a61ca18ec1cbf3
paulina koch <pjassakoch@gmail.com>
2026-01-22 14:52:29 -0500

    Added a playwright test for registering a new user

---
+++ e2e/auth.flow.spec.js
@@ -0,0 +1,63 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Authentication Lifecycle: Postman Flow', async ({request}) => {
+
+
+
+// ----- ARRANGE -----
+const timestamp = Date.now();
+const testUser = {
+  firstName: "Junior",
+  lastName: "Engineer",
+  email: `candidate_${timestamp}@work.com`,
+  password: "SecurePassword123"
+};
+
+let token;
+
+//----- STEP 1: REGISTER -----
+  await test.step('Step 1: Register a new User', async () => {
+    // ACT
+    const response = await request.post('/api/users/register', {
+      data: testUser
+    });
+
+    // ASSERT
+    expect(response.status()).toBe(201);
+    const body = await response.json();
+    expect(body.message).toBe("User registered successfully");
+  });
+
+
+  // ----- STEP 2: LOGIN -----
+  await test.step('Step 2: Login and Capture JWT', async ()=> {
+    //ACT
+const response = await request.post('/api/users/login', {
+    data: {
+       email: testUser.email,
+       password: testUser.password
+    }
+} );
+
+//ASSERT
+expect(response.status()).toBe(200);
+const body = await response.json();
+
+expect(body).toHaveProperty('token')
+token = body.token //Save it for the next "Act"
+expect(token).toBeDefined();
+});
+
+// ----- STEP 3: ACCESS PROTECTED ROUTE ----- 
+await test.step('Step 3: Access a protected route with the token', async () => {
+    const response = await request.get('/api/users/me', {
+            headers: {
+'Authorization': `Bearer ${token}`
+            }
+        })
+        expect(response.status()).toBe(200);
+        const body = await response.json();
+        expect(body.email).toBe(testUser.email);
+    })
+});
